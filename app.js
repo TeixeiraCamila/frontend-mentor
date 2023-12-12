fdd43fcc1fbc165c48656a6b354e1dee4e2b1919
CTeixeira <camila.teixeira@gmail.com.br>
2023-12-12 18:13:10 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+document.querySelector('footer').innerHTML = `
+<div class="attribution">
+Challenge by
+<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+  >Frontend Mentor</a
+>. Coded by
+<a target="_blank" href="https://www.frontendmentor.io/profile/TeixeiraCamila">Camila</a>.
+</div>
+<div class="attribution goHome">
+<a href="../../index.html">back to home</a>
+</div>
+`;
