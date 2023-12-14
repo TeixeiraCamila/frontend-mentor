d1b41c069f0de738b33ef2498c7f59c64dec396e
CTeixeira <camila.teixeira@gmail.com.br>
2023-12-14 20:18:12 -0300

    Challenge 04

---
+++ pages/__04__results-summary-component-main/assets/scripts/app.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function () {
+  fetchData();
+});
+
+async function fetchData() {
+  const response = await fetch(
+    '/assets/data.json'
+  );
+  const data = await response.json();
+  setData(data);
+}
+let list = document.querySelector('.summary__list');
+function setData(data) {
+  let string = ``;
+  data.forEach((el) => {
+    string += `
+      <div class="summary__item" style="background: var(--${el.category}__bg)"> 
+        <img src="${el.icon}" alt="${el.category}">
+        <p style="color: var(--${el.category}__color)"> ${el.category} </p>
+        <span> <span> ${el.score} </span> / 100 </span>
+      </div>
+    `;
+  });
+  list.innerHTML = string;
+}
