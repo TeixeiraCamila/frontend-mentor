a0e1c3b17e92a80116ab070c10f382faf528977b
CTeixeira <camila.teixeira@gmail.com.br>
2023-12-18 15:20:55 -0300

    [04] ajusta css geral

--- pages/__04__results-summary-component-main/assets/src/scripts/app.js
+++ pages/__04__results-summary-component-main/assets/src/scripts/app.js
@@ -1,21 +1,18 @@
 window.addEventListener('load', function () {
   fetchData();
 });
+let list = document.querySelector('.summary__list');
 
 async function fetchData() {
   const response = await fetch(
     '/pages/__04__results-summary-component-main/assets/data.json'
   );
   const data = await response.json();
-  setData(data);
-}
-let list = document.querySelector('.summary__list');
-function setData(data) {
   let string = ``;
   data.forEach((el) => {
     string += `
       <div class="summary__item" style="background: var(--${el.category}__bg)"> 
-        <img src="${el.icon}" alt="${el.category}">
+        <img src="./assets/src/img/${el.icon}" alt="${el.category}">
         <p style="color: var(--${el.category}__color)"> ${el.category} </p>
         <span> <span> ${el.score} </span> / 100 </span>
       </div>
