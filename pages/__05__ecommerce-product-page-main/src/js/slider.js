e7925c87f494caf0e9ab0931a38c913f5b10b061
CTeixeira <camila.teixeira@gmail.com.br>
2023-12-23 20:11:37 -0300

    teste slider

--- pages/__05__ecommerce-product-page-main/src/js/slider.js
+++ pages/__05__ecommerce-product-page-main/src/js/slider.js
@@ -1,6 +1,6 @@
 import { Fancybox } from '@fancyapps/ui';
 import { Carousel } from '@fancyapps/ui/dist/carousel/carousel.esm';
-import { Autoplay } from '@fancyapps/ui/dist/carousel/carousel.autoplay.esm.js';
+import { Thumbs } from '@fancyapps/ui/dist/carousel/carousel.thumbs.esm';
 
 export default function initSlider() {
   Fancybox.bind('[data-fancybox="gallery"]', {
@@ -9,25 +9,12 @@ export default function initSlider() {
     },
   });
 
-  let container;
-  let options;
-  if (window.innerWidth > 600) {
-    container = document.querySelector('.slider__main--desk');
-    options = {
-      infinite: true,
-      Autoplay: {
-        timeout: 3000,
-      },
-      Dots: false,
-    };
-    new Carousel(container, options, { Autoplay });
-    return;
-  } else {
-    container = document.querySelector('.slider__main--mob');
-    options = {
-      infinite: true,
-      Dots: false,
-    };
-    new Carousel(container, options);
-  }
+  let container = document.querySelector('#slider__container');
+  let options = {
+    Dots: false,
+    Thumbs: {
+      type: 'classic',
+    },
+  };
+  new Carousel(container, options, { Thumbs });
 }
