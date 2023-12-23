1d65bddb7d5db6fa1dc8058f202c0083836dd20a
CTeixeira <camila.teixeira@gmail.com.br>
2023-12-23 19:15:11 -0300

    atualiza main

--- pages/__05__ecommerce-product-page-main/src/js/slider.js
+++ pages/__05__ecommerce-product-page-main/src/js/slider.js
@@ -1,5 +1,6 @@
 import { Fancybox } from '@fancyapps/ui';
 import { Carousel } from '@fancyapps/ui/dist/carousel/carousel.esm';
+import { Autoplay } from '@fancyapps/ui/dist/carousel/carousel.autoplay.esm.js';
 
 export default function initSlider() {
   Fancybox.bind('[data-fancybox="gallery"]', {
@@ -7,8 +8,26 @@ export default function initSlider() {
       type: 'classic',
     },
   });
-  const container = document.querySelector('.slider__main');
-  const options = { infinite: false, dotTpl: false };
 
-  new Carousel(container, options);
+  let container;
+  let options;
+  if (window.innerWidth > 600) {
+    container = document.querySelector('.slider__main--desk');
+    options = {
+      infinite: true,
+      Autoplay: {
+        timeout: 3000,
+      },
+      Dots: false,
+    };
+    new Carousel(container, options, { Autoplay });
+    return;
+  } else {
+    container = document.querySelector('.slider__main--mob');
+    options = {
+      infinite: true,
+      Dots: false,
+    };
+    new Carousel(container, options);
+  }
 }
