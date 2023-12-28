b082a57b85edb4841c87773133bb21c5b4b6208c
CTeixeira <camila.teixeira@gmail.com.br>
2023-12-28 18:09:43 -0300

    ajuste resets css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,4 @@
-// document.querySelector('footer').innerHTML = `
-// <div class="attribution">
-// Challenge by
-// <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-//   >Frontend Mentor</a
-// >. Coded by
-// <a target="_blank" href="https://www.frontendmentor.io/profile/TeixeiraCamila">Camila</a>.
-// </div>
-// <div class="attribution goHome">
-// <a href="../../index.html">back to home</a>
-// </div>
-// `;
+
 const AppHeader = {
   name: 'AppHeader',
   template: '#appHeader',
@@ -41,7 +30,6 @@ new Vue({
       try {
         const response = await fetch('../../assets/js/cards.json');
         const data = await response.json();
-        console.log('🚀 ~ file: app.js:44 ~ fetchData ~ data:', data)
         setTimeout(() => {
           this.cards = data;
         }, 1500);
--- pages/__04__results-summary-component-main/src/assets/scripts/app.js
+++ pages/__04__results-summary-component-main/src/assets/scripts/app.js
@@ -2,17 +2,35 @@ window.addEventListener('load', function () {
   fetchData();
 });
 let list = document.querySelector('.summary__list');
-
-async function fetchData() {
-  const response = await fetch(
-    '/pages/__04__results-summary-component-main/assets/data.json'
-  );
-  const data = await response.json();
+const data = [
+  {
+    category: 'Reaction',
+    score: 80,
+    icon: 'icon-reaction.svg',
+  },
+  {
+    category: 'Memory',
+    score: 92,
+    icon: 'icon-memory.svg',
+  },
+  {
+    category: 'Verbal',
+    score: 61,
+    icon: 'icon-verbal.svg',
+  },
+  {
+    category: 'Visual',
+    score: 72,
+    icon: 'icon-visual.svg',
+  },
+];
+function fetchData() {
+  
   let string = ``;
   data.forEach((el) => {
     string += `
       <div class="summary__item" style="background: var(--${el.category}__bg)"> 
-        <img src="./assets/src/img/${el.icon}" alt="${el.category}">
+        <img src="./src/assets/img/${el.icon}" alt="${el.category}">
         <p style="color: var(--${el.category}__color)"> ${el.category} </p>
         <span> <span> ${el.score} </span> / 100 </span>
       </div>
